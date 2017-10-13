70e5bc2d72f39e50ceb79d342f64a998e0636971
Tran <r8tran@gmail.com>
2017-10-12 20:05:34 -0700

    update the params for flickr method call

--- safaribot.py
+++ safaribot.py
@@ -20,36 +20,39 @@ class SafariBot(object):
         self.__listener = None
         self.__imgsegmenter = ImageSegmenter()
         self.__clf = None
-    
+
     def listen_for_tweets(self):
         """Initializes a stream listener for the bot"""
         self.__listener = Listener(self.__api)
-        tweepy.Stream(self.__auth, self.__listener).filter(track=['@SafariParkBot'], async = True)
-    
+        tweepy.Stream(self.__auth, self.__listener).filter(track=['@SafariParkBot'], async=True)
+
     def post_animal_picture(self):
         """Uses Twitter API to update timeline with a new picture"""
         url = 'https://api.flickr.com/services/rest/'
         params = dict(
             api_key=FLICKR_API_KEY,
-            text ='animals',
+            text='safari',
+            tags='animals',
             format='json',
             nojsoncallback=1,
             method='flickr.photos.search',
         )
-        
-        r = requests.get(url, params=params)
-        photos = r.json()['photos']['photo']
-        photo = random.choice(photos)
-        url =  "https://farm{}.staticflickr.com/{}/{}_{}.jpg".format(str(photo['farm']), photo['server'], photo['id'], photo['secret'])
-        orig = "orig.png"
-        new = "new.png"
-        resp = requests.get(url, stream=True)
+
+        resp = requests.get(url, params=params)
         if resp.status_code == 200:
-            i = io.imread(BytesIO(resp.content))
-            io.imsave(orig, i)
-            img = self.__imgsegmenter.transform(i)
-            io.imsave(new, img)
-        self.__api.update_with_media(new, status=url)
+            photos = resp.json()['photos']['photo']
+            photo = random.choice(photos)
+            url =  "https://farm{}.staticflickr.com/{}/{}_{}.jpg".format(str(photo['farm']), photo['server'], photo['id'], photo['secret'])
+
+            orig = "orig.png"
+            new = "new.png"
+            resp = requests.get(url, stream=True)
+            if resp.status_code == 200:
+                i = io.imread(BytesIO(resp.content))
+                io.imsave(orig, i)
+                img = self.__imgsegmenter.transform(i)
+                io.imsave(new, img)
+                self.__api.update_with_media(new, status=url)
 
 
 if __name__ == '__main__':
